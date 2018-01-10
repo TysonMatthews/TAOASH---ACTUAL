fccc759eaf4881c30338df7104da1dc96b45f97e
AdamMorris17 <35115567+AdamMorris17@users.noreply.github.com>
2018-01-10 13:43:42 -0600

    1/10/18 - A
    Complete Pause Menu Build

--- The Adventures of a Space Hamster/Assets/Scripts/PauseMenu.cs
+++ The Adventures of a Space Hamster/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -17,19 +18,35 @@ public class PauseMenu : MonoBehaviour {
 				Resume ();
 			} else
 			{
-				PauseMenu ();
+				Pause();
 			}
 		}
 	}
 	
-	void Resume ()
+	public void Resume ()
 	{
 		pauseMenuUI.SetActive (false);
 		Time.timeScale = 1f;
 		GameIsPaused = false;
 	}
-	+
+
 	void Pause ()
 	{
-		pauseMenuUI
+		pauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene ("Main Menu");
 	
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("Quitting game...");
+		Application.Quit ();
+	}
+}
