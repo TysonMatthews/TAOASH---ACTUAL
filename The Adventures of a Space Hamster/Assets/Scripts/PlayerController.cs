4f09a041ca2bc475f3b8439b74aaac83ea0b4668
TysonMatthews <35093010+TysonMatthews@users.noreply.github.com>
2018-01-25 13:12:55 -0600

    Revert "1/22/18 - T"
    This reverts commit 2210004859b281ae14047bbebba38ef1873dd56a.

--- The Adventures of a Space Hamster/Assets/Scripts/PlayerController.cs
+++ The Adventures of a Space Hamster/Assets/Scripts/PlayerController.cs
@@ -30,9 +30,12 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		//Gets stated component
 		Anim = GetComponent<Animator> ();
+<<<<<<< HEAD
 		C2D = GetComponent<Controller2D> ();
 		Plyr = GetComponent<Player> ();
 
+=======
+>>>>>>> parent of 2210004... 1/22/18 - T
 
 		respawnPosition = transform.position;
 
@@ -40,7 +43,26 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void Update () {
+<<<<<<< HEAD
 		
+=======
+		//Checks if the player is touching the ground so that it can jump
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, WhatIsGround);
+
+		//Walking Mechanics
+		if(Input.GetAxisRaw ("Horizontal") > 0f) {
+			rb2d.velocity = new Vector3 (speed, rb2d.velocity.y, 0f);
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			rb2d.velocity = new Vector3 (-speed, rb2d.velocity.y, 0f);
+			transform.localScale = new Vector3 (-1, 1f, 1f);
+		} else {
+			rb2d.velocity = new Vector3 (0f, rb2d.velocity.y, 0f);
+		}
+
+		Jump ();
+
+>>>>>>> parent of 2210004... 1/22/18 - T
 		//Animations
 		Anim.SetFloat ("Speed", Mathf.Abs (Plyr.velocity.x));
 		Anim.SetBool ("Grounded", C2D.collisions.below);
